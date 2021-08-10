664ea06dc88dfdc6cfeeb32a84cc544a27590a98
Tyrel Chambers <tychambers3@gmail.com>
2021-08-10 00:27:11 +0000

    .

--- api/pets.js
+++ api/pets.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('../models/index')
 const authHandler = require('../middleware/authHandler')
 const app = express.Router()
+
 app.post('/v1/new', authHandler, async (req, res, next) => {
   try {
     const {name, breed} = req.body
