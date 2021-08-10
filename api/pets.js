797885f2ae4183fbc095ee12d67a40cba06d6871
Tyrel Chambers <tychambers3@gmail.com>
2021-08-10 00:22:30 +0000

    add pet route

--- api/pets.js
+++ api/pets.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const db = require('../models/index')
+const authHandler = require('../middleware/authHandler')
 const app = express.Router()
-
 app.post('/v1/new', authHandler, async (req, res, next) => {
   try {
-    
+    const {name, breed} = req.body
+    const pet = await db.models.Pet.create({
+      name,
+      breed
+    })
+    res.send(pet)
   } catch (error) {
     next(error)
   }
